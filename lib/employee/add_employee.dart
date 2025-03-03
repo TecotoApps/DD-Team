be16192fe3a2eb2059f08deded6e65f79e18ad58
Tilak <tilak@Tilaks-MacBook-Pro.local>
2025-03-03 22:39:09 +0530

    ADD employee code commit

--- lib/employee/add_employee.dart
+++ lib/employee/add_employee.dart
@@ -1,3 +1,5 @@
+import 'package:dd_shop/dashboard/dashboard_controller.dart';
+import 'package:dd_shop/dashboard/model/add_employee.dart';
 import 'package:dd_shop/utils/components/elevated_rounded_button.dart';
 import 'package:dd_shop/utils/components/text_field_curved_edges.dart';
 import 'package:dd_shop/utils/constants/colors.dart';
@@ -7,109 +9,177 @@ import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import '../utils/constants/app_fonts.dart';
 
-class AddEmployee extends StatelessWidget {
+class AddEmployee extends StatefulWidget {
+  @override
+  _AddEmployeeState createState() => _AddEmployeeState();
+}
+
+class _AddEmployeeState extends State<AddEmployee> {
   TextEditingController nameController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   DateTime? selectedDates;
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime now = DateTime.now();
+    DateTime today = DateTime(now.year, now.month, now.day);
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDates ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: today,
+    );
+
+    if (pickedDate != null && pickedDate != selectedDates) {
+      setState(() {
+        selectedDates = pickedDate;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: AppColors.appPrimaryColor,
-        title: Text('Add Employee',style: TextStyle(
+        title: Text('Add Employee', style: TextStyle(
             color: AppColors.textColor,
             fontSize: 20,
             fontFamily: 'OpenSansSemiBold')),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Employee Name',style: AppFonts.title.copyWith(
+            Text('Employee Name', style: AppFonts.title.copyWith(
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w500,
-                fontSize: 14),),
+                fontSize: 14)),
             Gap(8),
-            TextFieldCurvedEdges(controller: nameController,
+            TextFieldCurvedEdges(
+                controller: nameController,
                 backgroundColor: Colors.white,
                 borderRadius: 8,
-                keyboardType: TextInputType.name, borderColor: AppColors.card_border_color),
+                keyboardType: TextInputType.name,
+                borderColor: AppColors.card_border_color),
             Gap(8),
-            Text('Phone Number',style: AppFonts.title.copyWith(
+            Text('Phone Number', style: AppFonts.title.copyWith(
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w500,
-                fontSize: 14),),
+                fontSize: 14)),
             Gap(8),
-            TextFieldCurvedEdges(controller: phoneController,
+            TextFieldCurvedEdges(
+                controller: phoneController,
                 backgroundColor: Colors.white,
                 borderRadius: 8,
-                keyboardType: TextInputType.number, borderColor: AppColors.card_border_color),
+                keyboardType: TextInputType.number,
+                borderColor: AppColors.card_border_color),
             Gap(8),
-            Text('Email',style: AppFonts.title.copyWith(
+            Text('Email', style: AppFonts.title.copyWith(
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w500,
-                fontSize: 14),),
+                fontSize: 14)),
             Gap(8),
-            TextFieldCurvedEdges(controller: emailController,
+            TextFieldCurvedEdges(
+                controller: emailController,
                 backgroundColor: Colors.white,
                 borderRadius: 8,
-                keyboardType: TextInputType.emailAddress, borderColor: AppColors.card_border_color),
+                keyboardType: TextInputType.emailAddress,
+                borderColor: AppColors.card_border_color),
             Gap(8),
-            Text('Description',style: AppFonts.title.copyWith(
+            Text('Description', style: AppFonts.title.copyWith(
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w500,
-                fontSize: 14),),
+                fontSize: 14)),
             Gap(8),
-            TextFieldCurvedEdges(controller: emailController,
+            TextFieldCurvedEdges(
+                  controller: descriptionController,
                 backgroundColor: Colors.white,
                 borderRadius: 8,
-                keyboardType: TextInputType.text, borderColor: AppColors.card_border_color),
+                keyboardType: TextInputType.text,
+                borderColor: AppColors.card_border_color),
             Gap(8),
 
-            Text('Joining Date',style: AppFonts.title.copyWith(
+            Text('Joining Date', style: AppFonts.title.copyWith(
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w500,
-                fontSize: 14),),
+                fontSize: 14)),
             Gap(8),
             Card(
               color: Colors.white,
               elevation: 0,
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(selectedDates != null ? DateFormat('yyyy-MM-dd').format(selectedDates!) : 'Select Date'),
-                    IconButton(onPressed: (){
-
-                    }, icon: Icon(Icons.date_range,color: AppColors.appPrimaryColor,))
+                    Text(
+                      selectedDates != null
+                          ? DateFormat('yyyy-MM-dd').format(selectedDates!)
+                          : 'Select Date',
+                      style: TextStyle(fontSize: 16, color: Colors.black54),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                            _selectDate(context);
+                        },
+                        icon: Icon(Icons.date_range, color: AppColors.appPrimaryColor))
                   ],
                 ),
               ),
               shape: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                   borderSide: BorderSide(color: AppColors.card_border_color, width: 1)),
-
             ),
             Gap(8),
 
             Spacer(),
             RoundedElevatedButton(
-              width: MediaQuery.of(context).size.width, height: 40,
-              cornerRadius: 10,buttonColor: AppColors.appSecondaryColor,
-              onPressed: (){
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              cornerRadius: 10,
+              buttonColor: AppColors.appSecondaryColor,
+              onPressed: () async{
+                String name = nameController.text.trim();
+                String phone = phoneController.text.trim();
+                String email = emailController.text.trim();
+                String description = descriptionController.text.trim();
+                String joinDate = selectedDates != null ? DateFormat('yyyy-MM-dd').format(selectedDates!) : '';
+                if (name.isEmpty || phone.isEmpty || email.isEmpty || description.isEmpty || joinDate.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('All fields are required')),
+                  );
+                  return;
+                }
+
+                AddEmployeeModel addEmployeeModel= await DashboardController().addEmployeeByHR(
+                  name,
+                  phone,
+                  email,
+                  description,
+                  joinDate,
+                  context,
+                );
+
+                if (addEmployeeModel.statusCode == 201) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Employee added successfully')),
+                  );
+                  Navigator.pop(context); // Go back after successful submission
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to add employee')),
+                  );
+                }
 
               },
-              text:Dd_Strings.submit_button_text,
+              text: Dd_Strings.submit_button_text,
               textStyle: AppFonts.title.copyWith(color: Colors.white),
             ),
             Gap(8),
-
-
           ],
         ),
       ),
